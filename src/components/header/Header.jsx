7a1fd0ce9eda47a7c3e5d0d6108f708be2c0c028
dlozano-dawi <davidlozanocabeza@gmail.com>
2025-03-26 14:58:43 +0100

    Changed: new sidenav.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,47 +1,53 @@
 import React, { useContext } from 'react';
+import logOut from "../../assets/Icons/logOut.png"
+import userIcon from "../../assets/Icons/user.png"
 import { GlobalContext } from '../../Navigation';
 import { UserContext } from '../../Navigation';
 
-
-import './Header.css'
-import logOut from "../../assets/Icons/logOut.png"
-import userIcon from "../../assets/Icons/user.png"
-
 export const Header = () => {
-  const { setGlobalState } = useContext(GlobalContext);
-  const {  setUser } = useContext(UserContext);
+    const { setGlobalState } = useContext(GlobalContext);
+    const {  setUser } = useContext(UserContext);
 
-  function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
+    function clickNav() {
+        document.getElementById("mySidenav").classList.toggle("-translate-x-full");
+    }
 
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+    return (
+        <div className="flex justify-between items-center pl-4 w-full h-16">
+            {/* Menu Icon */}
+            <div className="flex flex-col space-y-1 cursor-pointer opacity-100 hover:opacity-60" onClick={clickNav}>
+                <div className="w-9 h-1 bg-black rounded-md transition duration-300"></div>
+                <div className="w-9 h-1 bg-black rounded-md transition duration-300"></div>
+                <div className="w-9 h-1 bg-black rounded-md transition duration-300"></div>
+            </div>
 
-  return (
-    <div className='header'>
-      <div className='menu' onClick={() => openNav()}> 
-        <div className="bar"></div>
-        <div className="bar"></div>
-        <div className="bar"></div> 
-      </div>
+            {/* Icons */}
+            <div className="flex space-x-4">
+                <img src={String(userIcon)} alt="User Icon" className="w-8 h-8 rounded-lg p-2 cursor-pointer hover:opacity-60"/>
+                <img src={String(logOut)} onClick={() => setUser(null)} alt="Log Out Icon" className="w-8 h-8 rounded-lg p-2 cursor-pointer hover:opacity-60"/>
+            </div>
 
-      <div className='icons'>
-        <img src={userIcon} className='icon' alt={""}/>
-        <img src={logOut} className='icon' onClick={() => setUser(null)}  alt={""}/>
-      </div>
-
-      <div id="mySidenav" className="sidenav">
-        <span className="closebtn" onClick={() => closeNav()}>&times;</span>
-        <span onClick={() => setGlobalState("Home")}>Home</span>
-        <span onClick={() => setGlobalState("Shop")}>Shop</span>
-        <span onClick={() => setGlobalState("Wishlist")}>Wishlist</span>
-        <span onClick={() => setGlobalState("Chats")}>Chats</span>
-        <span>Cart</span>
-        <span onClick={() => setGlobalState("Publish")}>Publish Product</span>
-        <span onClick={() => setGlobalState("Configuration")}>Settings</span>
-      </div>
-    </div>
-  )
+            {/* Sidebar Navigation */}
+            <div id="mySidenav" className="fixed top-0 left-0 h-full w-88 bg-white text-gray-600 transition-transform duration-500 -translate-x-full">
+                <span onClick={clickNav} className="absolute top-4 right-6 text-3xl cursor-pointer text-black">&times;</span>
+                <nav className="mt-16 flex flex-col text-xl">
+                    <div className='hover:bg-gray-100'>
+                        <div onClick={() => setGlobalState("Home")} className="pl-10 py-4 hover:text-black cursor-pointer transition-transform duration-500 hover:translate-x-[10%]">Home</div>
+                    </div>
+                    <div className='hover:bg-gray-100'>
+                        <div onClick={() => setGlobalState("Shop")} className="pl-10 py-4 hover:text-black cursor-pointer transition-transform duration-500 hover:translate-x-[10%]">Shop</div>
+                    </div>
+                    <div className='hover:bg-gray-100'>
+                        <div onClick={() => setGlobalState("Publish")} className="pl-10 py-4 hover:text-black cursor-pointer transition-transform duration-500 hover:translate-x-[10%]">Publish Product</div>
+                    </div>
+                    <div className='hover:bg-gray-100'>
+                        <div onClick={() => setGlobalState("Wishlist")} className="pl-10 py-4 hover:text-black cursor-pointer transition-transform duration-500 hover:translate-x-[10%]">Wishlist</div>
+                    </div>
+                    <div className='hover:bg-gray-100'>
+                        <div onClick={() => setGlobalState("Chats")} className="pl-10 py-4 hover:text-black cursor-pointer transition-transform duration-500 hover:translate-x-[10%]">Chats</div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    );
 }
