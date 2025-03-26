d7d7c36dfca644273eb74e458ded9c2226e8b44a
dlozano-dawi <davidlozanocabeza@gmail.com>
2025-03-26 21:06:31 +0100

    Added: Home components.

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import { Header } from "../header/Header";
 import bg from '../../assets/backgrounds/bg-table.png';
+import sf_logo from '../../assets/brand_logos/sf-logo.png';
 
 export const Home = () => {
     return (
-        <div className="w-screen h-screen bg-cover bg-no-repeat" style={{ backgroundImage: `url(${bg})` }}>
+        <div className="w-screen h-screen bg-cover bg-no-repeat bg-center bg-fixed" style={{ backgroundImage: `url(${bg})` }}>
             <Header/>
+            <main className="w-screen flex flex-col justify-center items-center mt-10">
+                <img src={String(sf_logo)} alt={'Sellfish logo'} className='w-120 h-auto'/>
+                <p className='w-170 text-center mt-10'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+                </p>
+            </main>
+
+
         </div>
     )
 }
