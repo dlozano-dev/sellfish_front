e87353c6ba885bed9676ea70d96ea10a4024e633
dlozano-dawi <davidlozanocabeza@gmail.com>
2025-04-21 01:45:59 +0200

    Added: Marking an item as favorite.

--- src/components/core/ItemComponent.tsx
+++ src/components/core/ItemComponent.tsx
@@ -1,6 +1,10 @@
 import {Clothe} from "../shop/data/Clothe.ts";
-import React from "react";
-import {SALE_STATES} from "../../utils/Constants.tsx";
+import React, {useContext, useEffect, useState} from "react";
+import {HOSTNAME, SALE_STATES} from "../../utils/Constants.tsx";
+import axios from "axios";
+import {UserIdContext} from "../../Navigation.tsx";
+import dagger_icon from "../../assets/Icons/dagger.svg";
+import dagger_bordered_icon from "../../assets/Icons/dagger_bordered.png";
 
 export const ItemComponent = ({
     setItem, item
@@ -8,21 +12,51 @@ export const ItemComponent = ({
     item: Clothe
     setItem: React.Dispatch<React.SetStateAction<Clothe | undefined>>;
 }) => {
+    const { userId } = useContext(UserIdContext)!;
+    const [fav, setFav] = useState(false);
+
+    async function setAsFavorite() {
+        const response = await axios.get(`${HOSTNAME}/liked/${userId}/${item!.id}`);
+        setFav(response.data)
+    }
+
+    useEffect(() => {
+        const checkFav = async () => {
+            const response = await axios.get(`${HOSTNAME}/isFav/${userId}/${item!.id}`);
+            setFav(response.data)
+        }
+
+        checkFav().then();
+    }, [userId, item]);
 
     return(
         <div
             onClick={() => setItem(item)}
             className="w-80 h-90 overflow-hidden rounded-md flex flex-col justify-end hover:cursor-pointer"
         >
-            <img
-                src={`data:image/png;base64,${item.picture}`}
-                alt={item.brand}
-                className="w-full h-full object-cover rounded-md"
-            />
+            {/* Image wrapper with relative positioning */}
+            <div className="relative w-full h-full">
+                <img
+                    src={`data:image/png;base64,${item.picture}`}
+                    alt={item.brand}
+                    className="w-full h-full object-cover rounded-md"
+                />
+
+                {/* Sword icon at bottom-right of the image */}
+                <img
+                    src={fav ? String(dagger_icon) : String(dagger_bordered_icon)}
+                    alt='Favorite Icon'
+                    onClick={(e) => {
+                        e.stopPropagation();
+                        setAsFavorite().then();
+                    }}
+                    className="w-16 h-16 cursor-pointer absolute bottom-2 right-0 z-10"
+                />
+            </div>
 
             <p className="py-2 text-gray-800 font-semibold items-start">
-                {item.saleState!==SALE_STATES[0].value ?
-                    <span className= {item.saleState===SALE_STATES[1].value ? 'text-purple-800' : 'text-green-700'}>
+                {item.saleState !== SALE_STATES[0].value ?
+                    <span className={item.saleState === SALE_STATES[1].value ? 'text-purple-800' : 'text-green-700'}>
                         {`[${item.saleState}] `}
                     </span>
                 :
